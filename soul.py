ab3a4ebb75494deb12d1267d8c618a45d41781ce
ivonic099 <glitchv97@gmail.com>
2025-05-22 18:33:03 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -5,7 +5,7 @@ import subprocess
 import datetime
 import os
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7174802055:AAFrmoLvSujgqkaBkZR2DO_wpxrJCVliigg')
+bot = telebot.TeleBot('7848896233:AAE7T7wPC_O8WGGFmgnL4kMS3MisPfN2qdc')
 
 # Admin user IDs
 admin_id = {"2057365092"}
